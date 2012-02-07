bbf64ef33b61bb23e9a6d91c23148386d0f0fda1
tl86 <tl86@duke.edu>
2012-02-07 00:08:41 -0500

    cla16bit ctrl_subtract removed

--- adder/CLA16bit.vhd
+++ adder/CLA16bit.vhd
@@ -4,7 +4,6 @@ use ieee.std_logic_1164.all;
 entity CLA16bit is
 	port (data_A, data_B : in std_logic_vector(15 downto 0);
 		  c_in : in std_logic;
-		  ctrl_subtract : in std_logic;
 		  sum : out std_logic_vector(16 downto 0);
 		  c_out : out std_logic);
 end CLA16bit;
@@ -15,7 +14,6 @@ component CLA8bit
 	port(data_A : in std_logic_vector(7 downto 0);
 		 data_B : in std_logic_vector(7 downto 0);
 		 c_in : in std_logic;
-		 ctrl_subtract : in std_logic;
 		 sum    : out std_logic_vector(7 downto 0);
 		 c_out  : out std_logic);
 end component;
@@ -24,7 +22,7 @@ signal c8 : std_logic;
 
 begin
 
-	bits70 : CLA8bit port map (data_A(7 downto 0), data_B(7 downto 0), c_in, ctrl_subtract, sum(7 downto 0), c8);
-	bits158 : CLA8bit port map (data_A(15 downto 8), data_B(15 downto 8), c8, ctrl_subtract, sum(15 downto 8), c_out);
+	bits70 : CLA8bit port map (data_A(7 downto 0), data_B(7 downto 0), c_in, sum(7 downto 0), c8);
+	bits158 : CLA8bit port map (data_A(15 downto 8), data_B(15 downto 8), c8, sum(15 downto 8), c_out);
 
 end structure;
